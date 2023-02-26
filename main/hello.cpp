5c4b30b2f816f0186dd5842d4fb6d0a3c97edeb5
ViRuS <67496726+GURAM-BALAJI@users.noreply.github.com>
2023-02-26 09:15:32 +0530

    first push

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "I have changed successfully\n";
+  return 0;
+}
---
+++ main/hello1.cpp
@@ -0,0 +1,8 @@
+//File: hello1.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+ cout << "PES2UG20CS805\n";
+  return 0;
+}
